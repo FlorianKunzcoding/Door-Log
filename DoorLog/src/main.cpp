155f40aef7ae4451f560b1eb86e9f7066a10cc48
FlorianKunz-coding <florian@kunzweb.net>
2026-06-12 07:46:54 +0200

    Created project framework

---
+++ DoorLog/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <MFRC522.h>
+#include <PIR.h>
+
+// Put pin definitions here:
+
+
+// put function declarations here:
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
